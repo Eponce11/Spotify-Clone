d872fa93231e5df8435309e82e0ca74c58207b87
Eric Ponce <ponceeric458@gmail.com>
2024-04-14 13:59:42 -0700

    jwt token middleware

--- Server/src/controllers/auth.controller.ts
+++ Server/src/controllers/auth.controller.ts
@@ -8,7 +8,6 @@ type ExpressRouteFunction = (
   res: Response
 ) => void | Promise<void>;
 
-
 export const register = (ctx: Context | MockContext): ExpressRouteFunction => {
   return async (req: Request, res: Response) => {
     try {
@@ -18,4 +17,20 @@ export const register = (ctx: Context | MockContext): ExpressRouteFunction => {
       res.sendStatus(400);
     }
   };
+};
+
+type TokenType = "ACCESS" | "REFRESH";
+
+const generateToken = (payload: { id: string }, tokenType: TokenType): any => {
+  if (tokenType === "ACCESS") {
+    const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
+    if (!accessTokenSecret) return null;
+    return jwt.sign(payload, accessTokenSecret, { expiresIn: "15m" });
+  }
+  if (tokenType === "REFRESH") {
+    const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
+    if (!refreshTokenSecret) return null;
+    return jwt.sign(payload, refreshTokenSecret, { expiresIn: "1d" });
+  }
+  return null;
 };
