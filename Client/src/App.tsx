4f59bec4e2621c225dd0b2072d8faf00d9c5a61a
Eric Ponce <ponceeric458@gmail.com>
2024-04-16 10:47:45 -0700

    set up home component

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,27 +1,23 @@
-
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import RequireAuth from './common/components/RequireAuth';
-import { Login, Register } from './pages';
+import RequireAuth from "./common/components/RequireAuth";
+import { Login, Register, Home } from "./pages";
 
 function App() {
-
   return (
-    <div className='App h-screen w-screen'>
+    <div className="App h-screen w-screen">
       <Router>
         <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
-          <Route path='/' element={<Login />}/>
-          <Route path='/register' element={<Register />}/>
-          
           <Route element={<RequireAuth />}>
-            <Route path='home/*' element={<div />} />
+            <Route path="home/*" element={<Home />} />
           </Route>
-
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
