7c3912dd547b555178fb78336cc60048da25fd32
Eric Ponce <ponceeric458@gmail.com>
2024-04-24 21:02:47 -0700

    play next and previous tracks hook

--- Client/src/common/hooks/index.ts
+++ Client/src/common/hooks/index.ts
@@ -1,5 +1,11 @@
 import useSpotifyRefreshToken from "./useSpotifyRefreshToken";
 import useTogglePlayback from "./useTogglePlayback";
 import usePlayTracks from "./usePlayTracks";
+import usePlayNextTrack from "./usePlayNextTrack";
 
-export { useSpotifyRefreshToken, useTogglePlayback, usePlayTracks };
+export {
+  useSpotifyRefreshToken,
+  useTogglePlayback,
+  usePlayTracks,
+  usePlayNextTrack,
+};
--- Client/src/pages/Home/components/BottomPlaybar.tsx
+++ Client/src/pages/Home/components/BottomPlaybar.tsx
@@ -1,14 +1,39 @@
-import { useAppSelector } from "../../../app/hooks";
-import { selectSpotifyPlaybackIsPlaying } from "../../../app/features/spotifyPlaybackSlice";
-import { useTogglePlayback } from "../../../common/hooks";
+import { useTogglePlayback, usePlayNextTrack } from "../../../common/hooks";
+import usePlayPreviousTrack from "../../../common/hooks/usePlayPreviousTrack";
 
 const BottomPlaybar = () => {
-  const isPlaying = useAppSelector(selectSpotifyPlaybackIsPlaying);
   const { togglePlayback } = useTogglePlayback();
+  const { playNextTrack } = usePlayNextTrack();
+  const { playPreviousTrack } = usePlayPreviousTrack();
 
   return (
-    <footer className="text-white h-[72px] bg-[red] py-2 px-3">
-      <div onClick={togglePlayback}>{isPlaying ? "Pause" : "Resume"}</div>
+    <footer className="text-white h-[72px] bg-[red] py-2 px-3 flex">
+      <div className="flex items-center w-1/3">
+        <div className="h-14 w-14 bg-[blue] rounded mr-3" />
+        <div className="flex flex-col gap-1">
+          <h5 className="text-h5">Title Name</h5>
+          <p className="text-h6">Artists</p>
+        </div>
+      </div>
+      <div className="h-full flex w-1/3 justify-center">
+        <div
+          className="h-8 w-8 flex justify-center items-center"
+          onClick={playPreviousTrack}
+        >
+          <div className="h-4 w-4 bg-[white]" />
+        </div>
+        <div
+          className="h-8 w-8 rounded-full bg-[white] mx-5"
+          onClick={togglePlayback}
+        />
+        <div
+          className="h-8 w-8 flex justify-center items-center"
+          onClick={playNextTrack}
+        >
+          <div className="h-4 w-4 bg-[white]" />
+        </div>
+      </div>
+      <div className="h-full w-1/3"></div>
     </footer>
   );
 };
