edc9677370b80db65d6939d9f96c17de571cb91a
Eric Ponce <ponceeric458@gmail.com>
2024-06-15 18:20:03 -0700

    validate email route

--- Server/src/routes/auth.routes.ts
+++ Server/src/routes/auth.routes.ts
@@ -3,6 +3,7 @@ import {
   register,
   login,
   handleRefreshToken,
+  registerEmailValidation,
 } from "../controllers/auth.controller";
 import { Context, MockContext } from "../config/context.config";
 
@@ -12,6 +13,7 @@ const authRoutes = (app: Application, database: Context | MockContext) => {
   app.post(`${baseAuthUrl}/register`, register(database));
   app.post(`${baseAuthUrl}/login`, login(database));
   app.get(`${baseAuthUrl}/refreshToken`, handleRefreshToken(database));
+  app.post(`${baseAuthUrl}/emailValidation`, registerEmailValidation(database));
 };
 
 export default authRoutes;
